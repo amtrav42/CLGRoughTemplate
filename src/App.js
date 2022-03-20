01f857bba528ca41eedafc70062d26131753d15a
katiepetersonn <katie.peterson19@gmail.com>
2022-03-20 16:23:48 +1100

    Add error message to a simple form

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SimpleInput from "./components/basicForm/SimpleForm";
+// import BasicForm from "./components/BasicForm";
+
+function App() {
+  return (
+    <div className="app">
+      {/* <BasicForm /> */}
+      <SimpleInput />
+    </div>
+  );
+}
+
+export default App;
